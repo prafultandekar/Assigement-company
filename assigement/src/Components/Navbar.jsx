cd4a38efac955f810280637884eebd1bb5c50bbb
prafultandekar <prafultandekar1997@gmail.com>
2022-11-06 16:35:53 +0530

    Update Navbar.jsx

--- assigement/src/Components/Navbar.jsx
+++ assigement/src/Components/Navbar.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 import {Link} from "react-router-dom"
 import style from "../Pages/style.module.css"
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
+// import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 function Navbar() {
   return (
     <div>
     
    <div className={style.font}>
    <div className={style.div}>
-   <span className={style.start} > <Link to="/"><i  class="fa-solid fa-star"></i></Link></span></div> 
+   <span className={style.start} > <Link ><i  class="fa-solid fa-star"></i></Link></span></div> 
 
    <div className={style.div}>
-   <span className={style.start} > <Link to="/"><i class="fa-light fa-person-biking"></i></Link> </span></div> 
+   <span className={style.start} > <Link ><i class="fa-light fa-person-biking"></i></Link> </span></div> 
 
     
     <div className={style.div}>
-   <span className={style.start} > <Link to="/"><i class="fa-solid fa-bicycle"></i></Link> </span></div> 
+   <span className={style.start} > <Link ><i class="fa-solid fa-bicycle"></i></Link> </span></div> 
 
    <div className={style.div}>
-   <span className={style.start} > <Link to="/"> <i class="fa-regular fa-person-skiing"></i></Link> </span></div> 
+   <span className={style.start} > <Link > <i class="fa-regular fa-person-skiing"></i></Link> </span></div> 
 
 
    <div className={style.div}>
-   <span className={style.start} > <Link to="/"><i class="fa-solid fa-person-skating"></i></Link> </span></div> 
+   <span className={style.start} > <Link ><i class="fa-solid fa-person-skating"></i></Link> </span></div> 
   
   
   
